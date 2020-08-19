7989b92bb66e6c4a7c8db985e1db43cdbf52c4b4
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-08-19 11:29:26 -0300

    att Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 
+// Importando o css
+import './styles.css';
+
 // Importando o ícone
 import naveLogo from '../../assets/images/icons/naveLogo.svg';
 
 function Login() {
     return(
-        <div id="login-page">
+        <div id="login-page" className="container">
             <div className="square-content">
-                <div className="square-header">
-                    <img src={naveLogo} alt="Nave"/>
-                </div>
-
-                <div className="form">
-                    <div className="input-block">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" id="email"/>
+                <main>
+                    <div className="square-header">
+                        <img src={naveLogo} alt="Nave"/>
                     </div>
-                </div>
+
+                    <div className="form">
+                        <div className="input-block">
+                            <label htmlFor="email">E-mail</label>
+                            <input placeholder="E-mail" type="text" id="email"/>
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="password">Senha</label>
+                            <input placeholder="Senha" type="text" id="password"/>
+                        </div>
+                    </div>
+
+                    <button>
+                        Entrar
+                    </button>
+                </main>
             </div>
         </div>
     )
