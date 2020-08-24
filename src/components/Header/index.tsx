44289f3ccb111f4e5c6868df40eb691d1c626dd6
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-08-23 21:44:55 -0300

    add theme option

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cookies from 'universal-cookie';
 
 // Importando o Link para fazermos a navegação
@@ -17,16 +17,38 @@ function Header() {
     function handleExit() {
         cookies.set('token', '');
     }
+
+    const [theme, setTheme] = useState(0);
+
+    useEffect(() => {
+        console.log('Tema alterado');
+
+        if(theme % 2 > 0){
+            document.documentElement.style.setProperty("--color-background", '#FFFF');    
+            document.documentElement.style.setProperty("--color-main-text", "#212121");
+            document.documentElement.style.setProperty("--filter-option", "0");
+        }
+        else {
+            document.documentElement.style.setProperty("--color-background", '#212121');    
+            document.documentElement.style.setProperty("--color-main-text", "#FFFF");
+            document.documentElement.style.setProperty("--filter-option", "10");
+        }
+            
+
+    }, [theme]);
+
     return(
         <header className="page-header">
             <Link to="/home">
                 <img src={naveLogo} alt="Nave"/>
             </Link>
             
+            <p onClick={() => setTheme(theme + 1)} >Mudar tema</p>
 
             <Link onClick={() => handleExit()} to="/">
                 Sair
             </Link>
+
         </header>
     )
 }
